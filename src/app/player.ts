0b422a1f9de30cc67bc2e93e4d29c85158f4c4c5
Tom <tomwansww@gmail.com>
2022-10-24 20:49:22 +0500

    Remove inappropriate declarations from player.ts

--- src/app/player.ts
+++ src/app/player.ts
@@ -1,21 +1,4 @@
-export type Row = 0 | 1 | 2;
-export type Column = 0 | 1 | 2;
-
-export const ROW_COUNT = 3;
-export const COLUMN_COUNT = 3;
-
-export interface Position {
-  row: Row;
-  column: Column;
-}
-
 export const enum PlayerFigure {
-  X,
-  O,
-}
-
-export const enum CellType {
-  EMPTY,
   X,
   O,
 }
