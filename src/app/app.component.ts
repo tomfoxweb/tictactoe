1dadc7a6fe1a85cfdd3e5a714b5b2870e425aa48
Tom <tomwansww@gmail.com>
2022-10-24 13:40:12 +0500

    Add ui for game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Cell {
+  row: number;
+  col: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tic Tac Toe';
+  cells: Cell[] = [];
+
+  ngOnInit(): void {
+    this.cells = [];
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        const cell: Cell = { row, col };
+        this.cells.push(cell);
+      }
+    }
+  }
 }
