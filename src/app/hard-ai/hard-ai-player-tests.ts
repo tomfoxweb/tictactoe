e72f923f674f7dad7c301d525289ea38669a6368
Tom <tomwansww@gmail.com>
2022-10-29 19:08:36 +0500

    Add tests for block horiz line

--- src/app/hard-ai/hard-ai-player-tests.ts
+++ src/app/hard-ai/hard-ai-player-tests.ts
@@ -16,4 +16,30 @@ export const hardAIPlayerTests: AIPlayerTest[] = [
     randomPosition: { row: 0, column: 2 },
     selectedPosition: { row: 0, column: 2 },
   },
+  {
+    title: 'should select cell for block opponent last on horiz line player O',
+    figure: PlayerFigure.O,
+    gameMap: [
+      [Cell.EMPTY, Cell.EMPTY, Cell.EMPTY],
+      [Cell.EMPTY, Cell.X, Cell.X],
+      [Cell.EMPTY, Cell.EMPTY, Cell.O],
+    ],
+    returnPosition: true,
+    returnCorner: false,
+    randomPosition: { row: 0, column: 2 },
+    selectedPosition: { row: 1, column: 0 },
+  },
+  {
+    title: 'should select cell for block opponent last on horiz line player X',
+    figure: PlayerFigure.X,
+    gameMap: [
+      [Cell.EMPTY, Cell.EMPTY, Cell.EMPTY],
+      [Cell.O, Cell.O, Cell.EMPTY],
+      [Cell.EMPTY, Cell.X, Cell.X],
+    ],
+    returnPosition: true,
+    returnCorner: false,
+    randomPosition: { row: 0, column: 2 },
+    selectedPosition: { row: 1, column: 2 },
+  },
 ];
